9660c9226e8eda20239c49d48ca26fff6fa38e04
VladimirKoryakovtsevI22 <157983129+VladimirKoryakovtsevI22@users.noreply.github.com>
2024-06-30 19:07:08 +0300

    Add files via upload

---
+++ Cort2/Cort2/Cort2.cpp
@@ -0,0 +1,48 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+void selectionSort(int a[], int N) {
+    for (int i = 0; i < N; i++) {
+        int max = i;
+        for (int j = i + 1; j < N; j++) {
+            if (a[j] > a[max])
+                max = j;
+        }
+        int temp = a[i];
+        a[i] = a[max];
+        a[max] = temp;
+    }
+}
+
+int main() {
+    srand(time(0));
+    int start = 0;
+    int end = 100;
+    int N;
+    printf("Enter massive arrea: ");
+    scanf_s ("%d", &N);
+
+    int* a = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % (end - start + 1) + start;
+    }
+
+    printf("First massive: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
+    selectionSort(a, N);
+
+    printf("Sort massive: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", a[i]);
+    }
+
+
+    return 0;
+}
